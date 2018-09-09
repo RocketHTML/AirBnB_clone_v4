cabaf20d47727d2b7c252a5a37b58c650bef5d56
koukijohn <koukijohn@github.com>
2018-09-09 20:06:40 +0000

    added web_dynamic/static/scripts/2-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
     });
   });
 
-  $.getJSON("http://0.0.0.0:5001/api/v1/status/", function (data, stats) {
+  $.get("http://0.0.0.0:5001/api/v1/status/", function (data, stats) {
     if (data.status === "OK") {
       $('#api_status').addClass('available');
     } else {
